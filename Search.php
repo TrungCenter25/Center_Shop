928efa1afe8aa7f5c10c7f9a6857d993849e7a17
TrungCenter25 <trungttgcc200091@fpt.edu.vn>
2022-10-19 08:15:25 +0700

    add

---
+++ Search.php
@@ -0,0 +1,46 @@
+<?php
+    if(isset($_POST['btnsearch']))
+    {
+        include_once("connection.php");
+        $se= $_POST['txtSearch'];
+        $result = mysqli_query($conn,"SELECT * from product where Product_Name like '%{$se}%'");
+    }
+    ?>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <h1>Search</h1>
+    <table id="tableproduct" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                    <th><strong>No.</strong></th>
+                    <th><strong>Product Name</strong></th>
+                    <th><strong>Price</strong></th>
+                    <th><strong>Image</strong></th>
+                </tr>
+             </thead>
+
+			<tbody>
+            <?php
+				$No=1;
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+                {
+			?>
+			<tr>
+              <td class="cotcheckbox"><?php echo $No; ?></td>
+              <td><?php echo $row["Product_Name"] ?></td>
+              <td><?php echo $row["Price"] ?></td>
+              <td align='center' class='columnfunction'>
+                        <img src='img/<?php echo $row["Pro_image"] ?>' border='0' width="50" height="50" />
+                        </td>
+                        <td align='center' class='columnfunction'>
+                        <a href="#" class="btn btn-warning" style="color:black">Buy</a>
+                        </td>
+                        
+            </tr>
+            <?php
+            $No++;
+                }
+			?>
+			</tbody>
+        
+        </table>  
---
+++ footer.php
@@ -0,0 +1,23 @@
+<div class="footer-top-area">
+        <div class="zigzag-bottom"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-about-us">
+                        <h2>Center<span> Store</span></h2>
+                        <p></p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-newsletter">
+                        <h2 class="footer-wid-title">Created by</h2>
+                        <p>Tran Thao Trung.</p>
+                        <p>0913573060.</p>
+                        <p>Trungttgcc200091@fpt.edu.vn.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
