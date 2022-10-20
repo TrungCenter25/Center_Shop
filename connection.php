5e293939de13ef560c92c656bb8a9c892e0564b7
TrungCenter25 <trungttgcc200091@fpt.edu.vn>
2022-10-20 19:27:39 +0700

    change

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-$Connect = pg_connect("postgres://dtmzealuackyzf:438ef65670da8a3baf15d52994c1ea75b6b8b154f6fd2d3b362599bce84417a9@ec2-34-200-205-45.compute-1.amazonaws.com:5432/d7t7l0kb2nt38l");	
-if (!$Connect) {
+$conn = pg_connect("postgres://dtmzealuackyzf:438ef65670da8a3baf15d52994c1ea75b6b8b154f6fd2d3b362599bce84417a9@ec2-34-200-205-45.compute-1.amazonaws.com:5432/d7t7l0kb2nt38l");	
+if (!$conn) {
     die("Connection failed");
 }
 ?>
